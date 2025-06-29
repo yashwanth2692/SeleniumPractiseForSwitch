b9e4e3a3aaeda4080efc06b22ecf7357a226f340
yashwanth2692 <105297630+yashwanth2692@users.noreply.github.com>
2025-06-29 16:07:46 +0530

    Changed Navigating link to makemytrip

--- src/main/java/org/practise/Selenium/GoogleSearchTest.java
+++ src/main/java/org/practise/Selenium/GoogleSearchTest.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import java.time.Duration;
 
 public class GoogleSearchTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new EdgeDriver();
 
-        driver.get("https://www.google.com");
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium WebDriver");
-        searchBox.submit();
+        driver.get("https://www.makemytrip.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath("//span[@class='commonModal__close']")).click();
+        driver.quit();
     }
 }
