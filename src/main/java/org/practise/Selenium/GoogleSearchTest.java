d63f302a09a5e20bdf7e4f798137ffddf128dab2
yashwanth2692 <105297630+yashwanth2692@users.noreply.github.com>
2025-06-29 15:38:46 +0530

    New Files

---
+++ src/main/java/org/practise/Selenium/GoogleSearchTest.java
@@ -0,0 +1,18 @@
+package org.practise.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class GoogleSearchTest {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+    }
+}
